75fe77d7b6f3609979aed374d7879970a8bb01ff
Antonette Caldwell <acald.nebula@gmail.com>
2023-12-30 09:10:20 -0600

    feat: revise schema to use the one from how to graphql

--- hackernews/graph/model/models_gen.go
+++ hackernews/graph/model/models_gen.go
@@ -2,16 +2,36 @@
 
 package model
 
-type NewTodo struct {
-	Text   string `json:"text"`
-	UserID string `json:"userId"`
+type Link struct {
+	ID      string `json:"id"`
+	Title   string `json:"title"`
+	Address string `json:"address"`
+	User    *User  `json:"user"`
 }
 
-type Todo struct {
-	ID   string `json:"id"`
-	Text string `json:"text"`
-	Done bool   `json:"done"`
-	User *User  `json:"user"`
+type Login struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Mutation struct {
+}
+
+type NewLink struct {
+	Title   string `json:"title"`
+	Address string `json:"address"`
+}
+
+type NewUser struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Query struct {
+}
+
+type RefreshTokenInput struct {
+	Token string `json:"token"`
 }
 
 type User struct {
